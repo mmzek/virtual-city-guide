b39c7579e99477f546a1c701296505478fcae1ff
Maja Mroczek <majamroczek.pl@gmail.com>
2025-07-28 22:46:54 +0200

    feat: set map location

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,17 +1,50 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+function SetViewOnLocation({ position }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (position) {
+      map.setView(position, 13);
+    }
+  }, [map, position]);
+
+  return null;
+}
+SetViewOnLocation.propTypes = { position: PropTypes.object.isRequired };
 
 function MapView() {
+  //Warsaw as a default
+  const [position, setPosition] = useState([52.2297, 21.0122]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        setPosition([latitude, longitude]);
+      },
+      (error) => {
+        console.error("Geolocation error:", error);
+      },
+    );
+  }, []);
+
   return (
     <MapContainer
-      center={[52.2297, 21.0122]}
-      zoom={5}
+      center={position}
+      zoom={13}
       style={{ height: "100%", width: "100%" }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+      <SetViewOnLocation position={position} />
+      <Marker position={position}></Marker>
     </MapContainer>
   );
 }
