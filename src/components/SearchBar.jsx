c3519d03f2f2e40e5b851fc6c36b3e8395ea3450
Maja Mroczek <majamroczek.pl@gmail.com>
2025-07-30 23:32:45 +0200

    feat: add weather

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,8 @@
 import { useState, useRef } from "react";
+import PropTypes from "prop-types";
 import LoadingBar from "react-top-loading-bar";
 import "./SearchBar.css";
-import "./MapView.jsx"
+import "./MapView.jsx";
 
 function SearchBar({ setPosition }) {
   const [query, setQuery] = useState("");
@@ -12,7 +13,7 @@ function SearchBar({ setPosition }) {
     loadingBarRef.current.continuousStart();
 
     const url = `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(
-      query
+      query,
     )}&apiKey=52bdc0d1cd0b477aa438a932e9aee7cd`;
 
     try {
@@ -21,7 +22,7 @@ function SearchBar({ setPosition }) {
 
       if (data.features.length > 0) {
         const { lat, lon } = data.features[0].properties;
-        setPosition([lat, lon]); 
+        setPosition([lat, lon]);
         console.log("Found:", lat, lon);
       } else {
         alert("Loction not found");
@@ -33,21 +34,25 @@ function SearchBar({ setPosition }) {
   };
   return (
     <div>
-       <LoadingBar color="lightpink" ref={loadingBarRef} />
-    <header className="header">Virtual City Guide</header>
-    <form>
-      <label className="label">
-        <input
-          value={query}
-          onChange={(q) => setQuery(q.target.value)}
-          className="search-input"
-          placeholder="Search location..."
-        />
-       <button className="button" onClick={handleSearch}>Search</button>
-      </label>
-    </form>
+      <LoadingBar color="lightpink" ref={loadingBarRef} />
+      <header className="header">Virtual City Guide</header>
+      <form>
+        <label className="label">
+          <input
+            value={query}
+            onChange={(q) => setQuery(q.target.value)}
+            className="search-input"
+            placeholder="Search location..."
+          />
+          <button className="button" onClick={handleSearch}>
+            Search
+          </button>
+        </label>
+      </form>
     </div>
   );
 }
-
+SearchBar.propTypes = {
+  setPosition: PropTypes.func.isRequired,
+};
 export default SearchBar;
--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -1,7 +1,81 @@
 import "./WeatherForecast.css";
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 
-function WeatherForecast(){
-    return(<div className="sidebar"><h1 className="h">Weather</h1>
-    </div>)
+function WeatherForecast({ position }) {
+  const [lat, lon] = position;
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    console.log("Position changed: " + position);
+    getWeather();
+  }, [position]);
+
+  const getWeather = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=fc090af6fb0559e067b747bd7a6f1372`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (!response.ok) {
+        console.error("Failed to fetch", response);
+        return null;
+      }
+
+      const timezone = data.timezone;
+      const localTime = new Date(Date.now() + timezone * 1000);
+
+      const weather = {
+        iconUrl: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+        temp: Math.round(data.main.temp - 272.15) + "°C",
+        feelsLike:
+          "Feels like: " + Math.round(data.main.feels_like - 272.15) + "°C",
+        time: localTime.toLocaleString(),
+        wind: "Wind speed: " + data.wind.speed + "km/h",
+        humidity: "Humidity: " + data.main.humidity + "%",
+        pressure: "Pressure: " + data.main.pressure + "mb",
+        cloudCover: "Cloud cover: " + data.clouds.all + "%",
+        description: data.weather[0].description,
+      };
+      setWeather(weather);
+    } catch (err) {
+      console.error("Qeury error:", err);
+    }
+  };
+
+  return (
+    <div className="sidebar">
+      <h1 className="h">Weather</h1>
+      <div className="row">
+        <div className="column">
+          <div className="row">
+            <h2 className="h2">{weather.temp}</h2>
+            <img src={weather.iconUrl} />
+          </div>
+          <div className="column" style={{ alignItems: "center" }}>
+            <h3 className="h3">{weather.feelsLike}</h3>
+            <h3 className="h3">{weather.time}</h3>
+          </div>
+        </div>
+        <div className="column">
+          <h4
+            className="h4"
+            style={{ color: "lightpink", fontSize: "large", marginTop: 20 }}
+          >
+            {weather.description}
+          </h4>
+          <h4 className="h4">{weather.wind}</h4>
+          <h4 className="h4">{weather.humidity}</h4>
+          <h4 className="h4">{weather.pressure}</h4>
+          <h4 className="h4">{weather.cloudCover}</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+WeatherForecast.propTypes = {
+  position: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
 export default WeatherForecast;
